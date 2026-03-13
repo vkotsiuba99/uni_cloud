09217aeb43e70715ce0f004b290ae869eeb5a9c2
Vladyslav Kotsiuba <92172262+vkotsiuba99@users.noreply.github.com>
2026-03-13 23:19:15 +0200

    fix ip

--- lab2/db.tf
+++ lab2/db.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "db_pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Basic"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "db_nic" {
--- lab3/storage_nodes.tf
+++ lab3/storage_nodes.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "stor_pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Basic"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "stor_nic" {
