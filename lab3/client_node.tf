d3b2db121f92fc068862d9f9b67c587b5310019d
Vladyslav Kotsiuba <92172262+vkotsiuba99@users.noreply.github.com>
2026-03-14 00:20:03 +0200

    fix generation

--- lab3/client_node.tf
+++ lab3/client_node.tf
@@ -40,7 +40,7 @@ resource "azurerm_linux_virtual_machine" "client" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
